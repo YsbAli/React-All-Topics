a0940323e713cc18162f958c793b156b5c60f121
YsbAli <yousub.webdev@gmail.com>
2023-07-18 12:28:53 +0530

    Pre Class

--- Day7(Forms & Refs)/Pre Class/src/App.jsx
+++ Day7(Forms & Refs)/Pre Class/src/App.jsx
@@ -1,13 +1,15 @@
-import "./App.css";
+import "./App.css"  ;
+import { Form } from "./Components/Forms";
 
 const App = () => {
   return (
     <>
       <div className="App">
-
+        <Form/>
       </div>
     </>
   );
 };
 
 export default App;
+ 
--- Day7(Forms & Refs)/Pre Class/src/Components/Forms.jsx
+++ Day7(Forms & Refs)/Pre Class/src/Components/Forms.jsx
@@ -1,15 +1,71 @@
+import { useState } from "react";
 
-export const Form = ()=>{
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-         
-         
+export const Form = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    age: "",
+  });
 
-        </form>
-        
-        
-        
-        </>
-    )
+
+  //collecting Data
+  const handleChange = (e) => {
+    // console.log(e);                     // e is here : -   SyntheticBaseEvent
+    // console.log(e.target)               //e.target is the input box itself
+
+    if(e.target.id === "username"){
+        setFormState({
+            ...formState,
+            username : e.target.value
+        })
+    }
+
+
+    if(e.target.id === "age"){
+        setFormState({
+            ...formState,
+            age : e.target.value
+        })
+    }
+
+
+
+  };
+
+  //Storing Data or Printing Data or  Storing Forms Data in Backend Server
+  const HandleForm = (e)=>{
+    e.preventDefault();                           // it will not refresh yourselft
+    console.log("Form Data", formState)           // printing the Data
+
+    // fetch("backend Api", {POST})                // backend api with post methods
+
+
 }
+
+
+
+  return (
+    <>
+      <form action="" onSubmit={HandleForm}>
+        {/* instead of id we can use name here to identify the perticuler inbox
+
+            <input type="text" id="username" placeholder="Enter Username" /> 
+
+             */}
+        <input
+          type="text"
+          onChange={handleChange}
+          id="username"
+          placeholder="Enter Username"
+        />
+        <input
+          type="number"
+          onChange={handleChange}
+          id="age"
+          placeholder="Enter Your Age"
+        />
+
+        <input type="Submit" />
+      </form>
+    </>
+  );
+};
