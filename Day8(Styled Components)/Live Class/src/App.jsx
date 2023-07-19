cab9df051e49fd62c0ea08db45ac412a16941335
YsbAli <yousub.webdev@gmail.com>
2023-07-20 01:56:19 +0530

    Own-Button-Components

--- Day8(Styled Components)/Live Class/src/App.jsx
+++ Day8(Styled Components)/Live Class/src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.css";
+import { Button } from "./Components/Button";
 
 const App = () => {
   return (
     <>
       <div className="App">
-        <h2> This is just like a Sample Project</h2>
+        <h2>Styled Components</h2>
+        {/* <Button  children={"Sign In"}/>
+        <Button children={"Sing Up"}/> */}
+
+        {/* we can pass like this also ... so, */}
+        {/* if we write like this <Button></Button>  --> it will automatically take its children */}
+{/* 
+        <Button
+          onClick={() => {
+            alert("Clicked on Sign In Button");
+          }}
+        >
+          Sign In
+        </Button> */}
+
+        
+        <Button
+          onClick={() => {
+            alert("Clicked on Click Me Button");
+          }}
+        >
+          Click Me
+        </Button>
+
+        <Button>Sign In</Button>
+        <Button>Register</Button>
       </div>
     </>
   );
---
+++ Day8(Styled Components)/Live Class/src/Components/Button.jsx
@@ -0,0 +1,34 @@
+// import "./Button.css";
+
+// export const Button = ({ children, onClick }) => {
+//   return (
+//     <>
+//       <button onClick={onClick} className="btn">
+//         {children}
+//       </button>
+//     </>
+//   );
+// };
+
+// aivabe amra annek gulo props nite pari for perticuler works,,,,
+// but this is not a fair way,,, so there styled-components comes into the pictures
+//but styled-components we can do as many things as we can
+
+// Lets See
+
+import styled from "styled-components";
+
+const Button = styled.button`
+  // here all the properties of button will present in Botton Component
+  border: none;
+  border-radius: 4px;
+  padding: 10px;
+  padding-top: 8px;
+  text-align: center;
+  background: #143f6b;
+  color: #f6f54d;
+  cursor: pointer;
+  margin: 5px;
+`;
+
+export { Button };
