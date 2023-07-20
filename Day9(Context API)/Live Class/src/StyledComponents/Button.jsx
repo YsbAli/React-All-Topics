d0414724b24db9a3045bbbe44ae99cb6cd7d0d2c
YsbAli <yousub.webdev@gmail.com>
2023-07-20 19:32:57 +0530

    Live Class

--- Day9(Context API)/Live Class/src/StyledComponents/Button.jsx
+++ Day9(Context API)/Live Class/src/StyledComponents/Button.jsx
@@ -9,4 +9,26 @@ export const MyButton = styled.button`
   &:hover {
     background-color: #7070ff;
   }
+
+  border: none;
+  border-radius: 4px;
+  padding: 13px;
+  text-align: center;
+  /* background: #143f6b; */
+  /* color: #f6f54d; */
+  background: ${(props)=>props.theme === "Light" ? "#f6f54D" : "#143f6b"};
+  color: ${(props)=> props.theme ==="Light" ? "#143f6b" : "#f6f54D"};
+  cursor: pointer;
+  margin: 5px;
+
+  //& refers to the current element  
+
+  &:hover {
+    background-color: blue;
+    box-shadow: #000000 0 3px 8px;
+  }
+
+
+
+
 `;
