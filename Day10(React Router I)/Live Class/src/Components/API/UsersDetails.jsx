162b4675c189300131f71fbd89292c6a4aae9ef8
YsbAli <yousub.webdev@gmail.com>
2023-07-25 23:53:24 +0530

    Reqres Api users data

--- Day10(React Router I)/Live Class/src/Components/API/UsersDetails.jsx
+++ Day10(React Router I)/Live Class/src/Components/API/UsersDetails.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const UsersDetails = () => {
   const { id } = useParams();
+  const [DataUsers, setUserData] = useState([]);
+
+  //calling api for perticular data on id
+
+  useEffect(() => {
+    axios.get(`https://reqres.in/api/users/${id}`).then(({ data }) => {
+      console.log("id and the Data:,", data);
+      setUserData(data.data);
+    });
+  }, [id]);
 
   return (
     <>
       <div>
-        <h2>Hello Users Details Page</h2>
-        <h3>Users Id : {id}</h3>
+        <h1>Hello : {id}</h1>
+        
+        {DataUsers.map((usersData) => (
+          <div key={usersData.id}>
+            <p>
+              <strong>
+                {usersData.first_name} {usersData.last_name}
+              </strong>
+            </p>
+            <h2>{usersData.email}</h2>
+            <img src={usersData.avatar} alt="" />
+          </div>
+        ))}
       </div>
     </>
   );
--- Day10(React Router I)/Live Class/src/Components/API/UsersList.jsx
+++ Day10(React Router I)/Live Class/src/Components/API/UsersList.jsx
@@ -1,49 +1,99 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { Link } from "react-router-dom";
+
+// const UsersList = () => {
+//   const [users, setUsers] = useState([]);
+
+//   // api : https://reqres.in/api/users?page=2
+//   // do api call with axios   -----> npm i axios
+
+//   //get request
+
+//   // useEffect(() => {
+//   //   axios.get("https://reqres.in/api/users").then(({ data }) => {
+//   //     console.log(data);
+//   //     setUsers(data.data);
+//   //   });
+//   // }, []);
+
+//   useEffect(() => {
+//     axios.get("https://reqres.in/api/users").then(({ data }) => {
+//       // console.log(data);
+//       setUsers(data.data);
+//     });
+//   }, []);
+
+//   return (
+//     <>
+//       <div>
+//         <h1>Users Page</h1>
+//         {users.map((user) => (
+//           // <div key={user.id}>
+//           //   {user.id}: {user.first_name}
+//           // </div>
+
+//           // new features is when i click on one name, all the details related to that name should be show,,let's see
+//           //for this we will use useParam hook and the route will be ---- users/:id
+
+//           // Now all we have to do is : use Link component in <users.firstname> and <user.lastname>
+//           // {/* on clicking user.first_name the Link will redirected to user.id,,, */}
+
+//           // <div key={user.i}>
+//           //   <div>
+//           //     <img src={user.avatar} alt="" />
+//           //   </div>
+//           //   <Link to={`/users/${user.id}`}>
+//           //     {user.id} : {user.first_name}
+//           //   </Link>
+//           // </div>
+
+//           // Now want to create Link tag in a image
+
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+// export default UsersList;
+
+// Practice and create with new features
+
 import axios from "axios";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const UsersList = () => {
-  const [users, setUsers] = useState([]);
-
-  // api : https://reqres.in/api/users?page=2
-  // do api call with axios   -----> npm i axios
-
-  //get request
-
-  // useEffect(() => {
-  //   axios.get("https://reqres.in/api/users").then(({ data }) => {
-  //     console.log(data);
-  //     setUsers(data.data);
-  //   });
-  // }, []);
-
-
+  const [userData, setUserData] = useState([]);
+  //Api Call
 
   useEffect(() => {
     axios.get("https://reqres.in/api/users").then(({ data }) => {
-      console.log(data);
-      setUsers(data.data);
+      console.log("Api Data", data);
+      setUserData(data.data);
     });
   }, []);
 
+  // Styling
+
+  const UserDiv = {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+  };
+
   return (
     <>
-      <div>
-        <h1>Users Page</h1>
-        {users.map((user) => (
-          // <div key={user.id}>
-          //   {user.id}: {user.first_name}
-          // </div>
+      <div style={UserDiv}>
+        {userData.map((apidata) => (
+          <div key={apidata.id}>
+            <p> {apidata.id}</p>
+            {/* <img src={apidata.avatar} alt="" /> */}
 
-          // new features is when i click on one name, all the details related to that name should be show,,let's see
-          //for this we will use useParam hook and the route will be ---- users/:id
-
-          // Now all we have to do is : use Link component in <users.firstname> and <user.lastname>
-
-          <div key={user.i}>
-            {/* on clicking user.first_name the Link will redirected to user.id,,, */}
-            <Link to={`/users/${user.id}`}>             
-              {user.id} : {user.first_name}
+            <Link to={`/users/${apidata.id}`}>
+              <img src={apidata.avatar} alt="" />
+              <h3>
+                {apidata.first_name} {apidata.last_name}
+              </h3>
             </Link>
           </div>
         ))}
@@ -51,4 +101,5 @@ const UsersList = () => {
     </>
   );
 };
+
 export default UsersList;
