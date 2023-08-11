e9a0f49903a8c14a5705bef0302075ea284a5f57
YsbAli <yousub.webdev@gmail.com>
2023-08-11 21:59:55 +0530

    state, initState, getState()

--- Day12(Redux-intro)/Redux/store.js
+++ Day12(Redux-intro)/Redux/store.js
@@ -1,26 +1,117 @@
 
 
-// import { legacy_createStore } from "redux"
+/*
 
-import { legacy_createStore  as createStore} from "redux"
+// // import { legacy_createStore } from "redux"
 
-// const store = legacy_createStore() 
+// import { legacy_createStore  as createStore} from "redux"
 
-const store = createStore()
+// // const store = legacy_createStore() 
 
+// const store = createStore()       // it will throw an error,,,,we have to pass the reducer function inside createStore,,,,,,the error is--- Expected the root reducer to be a function. Instead, received: '" +kindOf(reducer) + "'");
+
+
+// console.log(store)           // it will trow an error ,,, we have to define the module,, because we can't define import outside the module... so in package.json file just mention "type" : "module"
+
+
+
+
+// const Reducer = (store, action)=>{
+//     if(action.type === "ADD_COUNT"){
+//         return {...store, counter : store.counter + action.payload}
+//     }
+// }
+
+
+
+import { legacy_createStore as createStore } from 'redux'
+
+
+
+//reducer represents how to update the store
+const Reducer = (store, action) => {
+    if (action.type === "ADD_COUNT") {
+        return { ...store, counter: store.counter + action.payload }
+    }
+    // return { ...store }               // we have to always return something...   because reducer always return something,,,,
+    return store;
+}
+
+
+
+// this is our apps initial state,,, we have to define it,,, no matter how many variable or state are there
+
+// const initState = {
+//     counter: 10
+// }
+
+
+const initState = {
+    counter: 0,
+    todos: [],
+    theme: "dark",
+    price: 0
+}
+
+// const store = createStore(Reducer)     // via this store and reducer getting connected
+
+const store = createStore(Reducer, initState)
+
+// console.log(store, store.getState())      // getState()  ---> will always return the current state of the store or app
 console.log(store)
 
 
+console.log(store.getState())              // getState will always give the current state so the output will be ,,,, {counter : 0 }
+
+// the output is :
+
+// {
+//     dispatch: [Function: dispatch],
+//     subscribe: [Function: subscribe],
+//     getState: [Function: getState],
+//     replaceReducer: [Function: replaceReducer],
+//     '@@observable': [Function: observable]
+//   }
+
+// so in store we have those 4 function but we need only dispatch() and getState() 
 
 
 
 
+*/
 
 
 
 
-const Reducer = (store, action)=>{
-    if(action.type === "ADD_COUNT"){
-        return {...store, counter : store.counter + action.payload}
+// soo the fresh code is      ------
+
+
+import { legacy_createStore as createStore } from 'redux';
+
+
+const Reducer = (store, action) => {
+    if (action.type === "ADD_THEME") {
+        return { ...store, counter: store.counter + action.payload }
     }
+
+    return store;
 }
+
+
+const initState = {
+    counter: 0,
+    theme: 'dark',
+    todos: [],
+    price: 0
+}
+
+const store = createStore(Reducer, initState)
+console.log(store)           // output will be those four function
+
+
+console.log(store.getState())                        // { counter: 0, theme: 'dark', todos: [], price: 0 }
+
+
+
+
+
