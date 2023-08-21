0b51fa417eea84f79ef908e0499e0e1941ff2022
YsbAli <yousub.webdev@gmail.com>
2023-08-21 21:48:13 +0530

    useDispatch and useSelector

--- Day13(Redux II)/Live Sir/redux-1/src/Components/Counter.jsx
+++ Day13(Redux II)/Live Sir/redux-1/src/Components/Counter.jsx
@@ -1,18 +1,28 @@
 import { AddCount } from "../../Redux/Acton";
 import { Store } from "../../Redux/Store";
+import { useDispatch, useSelector } from "react-redux";
 
 function Counter() {
   console.log(Store.getState());
+  const dispatch = useDispatch();
+  //   const UpdatedCounter = useSelector((store) => store.counter);                    // getting entire state or store or entire data here...
+  //UpdatedCounter is the updated value of counter and the store.counter is the initial state or initial value of counter
+
+  //   const UpdatedCounter = useSelector((store) => {
+  //     return store.counter;
+  //   });
+
+  const UpdatedCounter = useSelector((state) => state.counter);
 
   const HandleAdd = () => {
-    Store.dispatch(AddCount(1));
+    // Store.dispatch(AddCount(1));
+    dispatch(AddCount(1));
   };
-
 
   return (
     <div>
-      <h3>Counter : 0</h3>
-      <h3>Counter : {Store.Counter}</h3>
+      <h3>Counter : {UpdatedCounter}</h3>
+      {/* <h3>Counter : {Store.getState().Counter}</h3> */}
       <button onClick={HandleAdd}>Count</button>
     </div>
   );
